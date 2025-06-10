a5440bcd213d1faf92c5d4f6a67dd36743567836
Viet Nguyen <vietnt94v@gmail.com>
2025-06-10 23:26:24 +0700

    feat: update position dropdown

--- src/components/ui/Dropdown/DropdownMenu.tsx
+++ src/components/ui/Dropdown/DropdownMenu.tsx
@@ -1,13 +1,63 @@
-import React, { useId, useEffect } from "react";
+import React, { useId, useEffect, useState } from "react";
 import { useDropdownContext } from "./DropdownContext";
+import { createPortal } from "react-dom";
 
 interface DropdownMenuProps {
   children: React.ReactNode;
+  spaceMenuToTrigger?: number;
 }
 
 const DropdownMenu: React.FC<DropdownMenuProps> = ({ children }) => {
-  const { isOpen, setIsOpen, menuRef } = useDropdownContext();
+  const { isOpen, setIsOpen, triggerRef, menuRef } = useDropdownContext();
   const id = useId();
+  const [position, setPosition] = useState<{ top: number; left: number }>({
+    top: 0,
+    left: 0,
+  });
+  const spaceMenuToTrigger = 8;
+
+  useEffect(() => {
+    if (isOpen && menuRef.current) {
+      menuRef.current.focus();
+    }
+  }, [isOpen, menuRef]);
+
+  useEffect(() => {
+    if (!isOpen) return;
+    if (!triggerRef.current || !menuRef.current) return;
+
+    const updatePosition = () => {
+      if (triggerRef?.current) {
+        const triggerRect = triggerRef.current.getBoundingClientRect();
+        const menuHeight = menuRef.current?.offsetHeight || 0;
+        const spaceMenuToBottom = window.innerHeight - triggerRect.bottom;
+        const spaceMenuToTop = triggerRect.top;
+
+        const shouldMenuOpenAbove =
+          spaceMenuToBottom < menuHeight - spaceMenuToTrigger &&
+          spaceMenuToTop > menuHeight + spaceMenuToTrigger;
+
+        let topPosition = shouldMenuOpenAbove
+          ? triggerRect.top - menuHeight + window.scrollY - spaceMenuToTrigger
+          : triggerRect.bottom + window.scrollY + spaceMenuToTrigger;
+
+        setPosition({
+          top: topPosition,
+          left: triggerRect.left + window.scrollX,
+        });
+      }
+    };
+
+    updatePosition();
+
+    window.addEventListener("scroll", updatePosition, true);
+    window.addEventListener("resize", updatePosition);
+
+    return () => {
+      window.removeEventListener("scroll", updatePosition, true);
+      window.removeEventListener("resize", updatePosition);
+    };
+  }, [isOpen, triggerRef]);
 
   const handleKeyDown = (e: React.KeyboardEvent) => {
     if (e.key === "Escape") {
@@ -16,26 +66,25 @@ const DropdownMenu: React.FC<DropdownMenuProps> = ({ children }) => {
     }
   };
 
-  useEffect(() => {
-    if (isOpen && menuRef.current) {
-      menuRef.current.focus();
-    }
-  }, [isOpen]);
-
   if (!isOpen) return null;
 
-  return (
+  return createPortal(
     <div
       id={`dropdown-menu-${id}`}
       role="menu"
       tabIndex={-1}
       ref={menuRef}
       onKeyDown={handleKeyDown}
-      className="absolute mt-2 w-48 rounded-md shadow-lg bg-stone-900 ring-1 ring-black ring-opacity-5 z-50"
-      style={{ top: "100%" }}
+      className="rounded-md shadow-lg bg-stone-900 ring-1 ring-stone-500/50 z-50"
+      style={{
+        position: "fixed",
+        top: position.top,
+        left: position.left,
+      }}
     >
       <div className="py-1">{children}</div>
-    </div>
+    </div>,
+    document.body
   );
 };
 
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,17 +3,23 @@ import { Button, Dropdown } from "@/components/ui";
 const Home = () => {
   return (
     <>
-      <h1>Home</h1>
+      <h1 className="text-5xl py-3">Home</h1>
       <Dropdown>
         <Dropdown.Trigger>
           <Button>Open Menu</Button>
         </Dropdown.Trigger>
         <Dropdown.Menu>
           <Dropdown.Item onClick={() => console.log("Item 1")}>
-            Item 1
+            Item option 1
           </Dropdown.Item>
           <Dropdown.Item onClick={() => console.log("Item 2")}>
-            Item 2
+            Item curent 2
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => console.log("Item 3")}>
+            Item curent spcial 3
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => console.log("Item 4")}>
+            Item curent the 4
           </Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
