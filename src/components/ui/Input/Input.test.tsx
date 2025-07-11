d85fa4f9d287d4bec60b41a6937eab0777dd46fe
vietnguyen <vietnguyen@fortna.com>
2025-07-11 17:09:02 +0700

    feat: update input test

---
+++ src/components/ui/Input/Input.test.tsx
@@ -0,0 +1,59 @@
+import { describe, expect, it, vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import Input from './Input';
+import userEvent from '@testing-library/user-event';
+
+describe('Input', () => {
+  it('should render input component', () => {
+    render(
+      <Input placeholder="Enter text" value={'John'} onChange={() => {}} />
+    );
+    const input = screen.getByPlaceholderText('Enter text') as HTMLInputElement;
+    expect(input).toBeInTheDocument();
+    expect(input).toHaveAttribute('placeholder', 'Enter text');
+    expect(input.value).toBe('John');
+  });
+
+  it('calls onChange when typing', async () => {
+    const user = userEvent.setup();
+    const handleChange = vi.fn();
+
+    render(<Input placeholder="Type here" value="" onChange={handleChange} />);
+
+    const input = screen.getByPlaceholderText('Type here') as HTMLInputElement;
+    await user.type(input, 'Hello');
+
+    expect(handleChange).toHaveBeenCalledTimes(5); // 5 characters typed in 'Hello'
+    expect(input.value).toBe('Hello');
+  });
+
+  it('can toggle password visibility', async () => {
+    const user = userEvent.setup();
+
+    render(
+      <Input
+        type="password"
+        placeholder="Enter password"
+        value=""
+        onChange={() => {}}
+        hasIconShowPassword
+      />
+    );
+
+    const input = screen.getByPlaceholderText(
+      'Enter password'
+    ) as HTMLInputElement;
+    const toggleButton = screen.getByRole('button');
+    const toggleIcon = toggleButton.querySelector('i');
+
+    expect(input.type).toBe('password');
+    expect(toggleIcon?.className).toBe('fa-solid fa-eye');
+    expect(toggleIcon).toHaveClass('fa-eye');
+
+    await user.click(toggleButton);
+    expect(input.type).toBe('text');
+
+    await user.click(toggleButton);
+    expect(input.type).toBe('password');
+  });
+});
