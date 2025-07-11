1ecf7b6db9cddd82200804b9d1e6b4e37272af94
vietnguyen <vietnguyen@fortna.com>
2025-07-11 17:23:00 +0700

    feat: update Input test

--- src/components/ui/Input/Input.test.tsx
+++ src/components/ui/Input/Input.test.tsx
@@ -1,7 +1,8 @@
 import { describe, expect, it, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import Input from './Input';
 import userEvent from '@testing-library/user-event';
+import Input from './Input';
+import type { ValidationRule } from './Input.types';
 
 describe('Input', () => {
   it('should render input component', () => {
@@ -55,5 +56,51 @@ describe('Input', () => {
 
     await user.click(toggleButton);
     expect(input.type).toBe('password');
+  });
+
+  it('calls onBlur and shows validation error', async () => {
+    const user = userEvent.setup();
+    const handleBlur = vi.fn();
+    const validationRules: ValidationRule[] = [
+      {
+        type: 'required',
+        message: 'This field is required',
+      },
+    ];
+
+    render(
+      <Input
+        placeholder="Required input"
+        value=""
+        onChange={() => {}}
+        onBlur={handleBlur}
+        validationRules={validationRules}
+      />
+    );
+
+    const input = screen.getByPlaceholderText(
+      'Required input'
+    ) as HTMLInputElement;
+
+    await user.click(input);
+    await user.tab();
+    expect(handleBlur).toHaveBeenCalled();
+    expect(screen.getByText('This field is required')).toBeInTheDocument();
+  });
+
+  it('is disabled when isDisabled prop is true', () => {
+    render(
+      <Input
+        placeholder="Disabled input"
+        value=""
+        onChange={() => {}}
+        isDisabled={true}
+      />
+    );
+
+    const input = screen.getByPlaceholderText(
+      'Disabled input'
+    ) as HTMLInputElement;
+    expect(input).toBeDisabled();
   });
 });
