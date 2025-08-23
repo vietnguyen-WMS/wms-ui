a1237c47fb4939b972c495def99554d1357d55f5
Viet Nguyen <11242644+vietnt94v@users.noreply.github.com>
2025-08-23 23:41:17 +0700

    refactor: load translations from json files

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,14 +1,39 @@
 import { Link } from 'react-router-dom';
 import { Dropdown } from '@/components/ui';
 import { useAuth } from '@/stores';
+import { useTranslation } from 'react-i18next';
 
 const Header = () => {
   const { user, logout } = useAuth();
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
+  };
 
   return (
     <>
       <div className="px-3 h-12 flex items-center justify-end bg-gray-500">
         <div className="flex items-center space-x-3">
+          <Dropdown>
+            <Dropdown.Trigger>
+              <button className="px-2 py-1 border rounded">
+                {i18n.language.toUpperCase()}
+              </button>
+            </Dropdown.Trigger>
+            <Dropdown.Menu>
+              <Dropdown.Item>
+                <button onClick={() => changeLanguage('en')}>
+                  {t('english')}
+                </button>
+              </Dropdown.Item>
+              <Dropdown.Item>
+                <button onClick={() => changeLanguage('vi')}>
+                  {t('vietnamese')}
+                </button>
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
           <Dropdown>
             <Dropdown.Trigger>
               <div className="w-8 h-8 rounded-full overflow-hidden">
@@ -41,12 +66,12 @@ const Header = () => {
               </div>
               <Dropdown.Item>
                 <Link to="/settings" className="block">
-                  Settings
+                  {t('settings')}
                 </Link>
               </Dropdown.Item>
               <Dropdown.Item>
                 <button className="block cursor-pointer" onClick={logout}>
-                  Logout
+                  {t('logout')}
                 </button>
               </Dropdown.Item>
             </Dropdown.Menu>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,12 @@
+import { useTranslation } from 'react-i18next';
+
 const Home = () => {
+  const { t } = useTranslation();
+
   return (
     <>
-      <h1>Home</h1>
+      <h1>{t('home')}</h1>
+      <p>{t('welcome')}</p>
     </>
   );
 };
