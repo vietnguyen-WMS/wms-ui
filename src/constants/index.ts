8bcd6cb39496c81615c93cab13b00b46897a1d47
Viet Nguyen <vietnt94v@gmail.com>
2025-08-23 08:32:28 +0700

    add comment

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,9 +2,13 @@ const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
 
 export const API = {
   URL: API_URL,
+
+  // auth
   AUTH_LOGIN: `${API_URL}/auth/login`,
   AUTH_LOGOUT: `${API_URL}/auth/logout`,
   AUTH_ME: `${API_URL}/auth/me`,
+
+  // users
   GET_USERS: `${API_URL}/users`,
   GET_USERS_BY_ID: (id: number) => `${API_URL}/users/${id}`,
   ADD_USER: `${API_URL}/users`,
