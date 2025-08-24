5a95496e8d8292582ccbf2fddde7a1e7d5a5a7b0
Viet Nguyen <11242644+vietnt94v@users.noreply.github.com>
2025-08-24 18:01:40 +0700

    fix: provide i18n stub for tests

--- src/mocks/react-i18next.ts
+++ src/mocks/react-i18next.ts
@@ -1,4 +1,14 @@
-export const useTranslation = () => ({ t: (key: string) => key });
+const i18n = {
+  language: 'en',
+  changeLanguage: async (lng: string) => {
+    i18n.language = lng;
+  },
+};
+
+export const useTranslation = () => ({
+  t: (key: string) => key,
+  i18n,
+});
 
 export const initReactI18next = {
   type: '3rdParty' as const,
