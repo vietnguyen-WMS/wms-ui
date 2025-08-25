3d990733c8f39ea24a1e37a5af2b7cc6064c451c
vietnguyen <vietnguyen@fortna.com>
2025-08-25 10:51:22 +0700

    update home test

--- src/pages/home/Home.test.tsx
+++ src/pages/home/Home.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { describe, it } from 'vitest';
+import { describe, it, expect } from 'vitest';
 import { ToastContainer } from '@components/ui/Toast';
 
 describe('Home', () => {
